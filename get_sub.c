033369ea1f48e7fec61a528d309f1c225ade4044
inyambeGoodness <goodnessjoker@gmail.com>
2023-06-26 00:04:42 +0000

    updated version of code for this project

--- get_sub.c
+++ get_sub.c
@@ -1,37 +1,32 @@
 #include "monty.h"
 
 /**
- * get_swap - function that swaps the top two elements on the stack
+ * get_sub - function that subtracts the top two elements of the stack
  * @stack: pointer to the top of the stack
  * @line_number: where the line number appears
- * Description: 3. swap
+i * Description: 6. sub
  * Return: see below
  * 1. upon success, nothing
  * 2. upon fail, EXIT_FAILURE
  */
-void get_swap(stack_t **stack, unsigned int line_number)
+void get_sub(stack_t **stack, unsigned int line_number)
 {
 	stack_t *first, *second;
 
 	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		fclose(file);
 		get_free(*stack);
 		exit(EXIT_FAILURE);
 	}
-	else if (*stack)
+	else
 	{
 		first = *stack;
 		second = first->next;
+		second->n -= first->n;
 		*stack = second;
-		first->next = second->next;
-		if (second->next != NULL)
-		{
-			second->next->prev = first;
-		}
-		second->prev = NULL;
-		second->next = first;
-		first->prev = second;
+		(*stack)->prev = NULL;
+		free(first);
 	}
 }
--- main.c
+++ main.c
@@ -1,45 +1,55 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+ *  main - Main
+ *
+ *  @argc: Number of args
+ *
+ *  @argv: Command line args
+ *
+ *  Return: Void
+ */
+
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	stack_t *head = NULL;
+	char  *str = NULL, *operator_array[2], *temp;
+	size_t bufsize = 1024, line_count = 0;
+	ssize_t get_line;
+	void (*operator_function)(stack_t **stack, unsigned int line_number);
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
 	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+	if (file == NULL)
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
+	while (1)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
+		get_line = getline(&str, &bufsize, file);
+		if (get_line == -1)
+			break;
+		line_count++;
+		operator_array[0] = strtok(str, "\n ");
+		if (operator_array[0] == NULL)
+			get_nop(&head, line_count);
+		else if (strcmp("push", operator_array[0]) == 0)
 		{
-			execute(content, &stack, counter, file);
+			temp = strtok(NULL, "\n ");
+
+			get_push(&head, line_count, temp);
 		}
-		free(content);
+		else if (operator_array[0] != NULL && operator_array[0][0] != '#')
+		{
+			operator_function = go(operator_array[0], line_count, &head);
+
+			if (operator_function == NULL && line_count == 0)
+			{
+				fprintf(stderr, "L%ld: unknown instruction %s\n",
+					line_count, operator_array[0]), exit(EXIT_FAILURE);
+			}
+		operator_function(&head, line_count);
+		}
 	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+	fclose(file), free(str), get_free(head);
+	return (0);
 }
